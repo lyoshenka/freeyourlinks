0975ee6f18c44aee6cfe48201b085fefc01cd2d3
Alex Grintsvayg <alex@flickswitch.cc>
2012-03-07 16:39:57 -0500

    getting it to work

--- index.php
+++ index.php
@@ -4,12 +4,20 @@
 //$phar->extractTo('./silex'); // extract all files
 
 //require_once __DIR__.'/silex.phar'; 
-require_once __DIR__ . '/silex/vendor/.composer/autoload.php';
+require_once __DIR__ . '/vendor/silex/vendor/.composer/autoload.php';
 
 $app = new Silex\Application(); 
+$app['debug'] = true;
 
-$app->get('/', function() use($app) { 
-  return 'Hello everyone!'; 
-}); 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path'       => __DIR__.'/views',
+  'twig.class_path' => __DIR__.'/vendor/twig/lib',
+));
+
+
+$app->get('/{url}', function() use($app) { 
+  return $app['twig']->render('home.twig');
+})->assert('url','.*'); 
+
 
 $app->run(); 
