21d62a01bf7faaf8cea5e4441dbafaa56fc231b7
Alex Grintsvayg <alex@flickswitch.cc>
2012-03-07 16:08:29 -0500

    better?

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/silex.phar';
 $app = new Silex\Application(); 
 
 $app->get('/', function() use($app) { 
-      return 'Hello world!'; 
+  return 'Hello everyone!'; 
 }); 
 
 $app->run(); 
