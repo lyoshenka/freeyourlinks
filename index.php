9bddc3c8ee6acbbad413132c6bbfe8c3fa7cf899
Alex Grintsvayg <alex@flickswitch.cc>
2012-03-07 16:04:28 -0500

    Added silex

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo "Hello world!"; ?>
+<?php
+require_once __DIR__.'/silex.phar'; 
+
+$app = new Silex\Application(); 
+
+$app->get('/', function() use($app) { 
+      return 'Hello world!'; 
+}); 
+
+$app->run(); 
