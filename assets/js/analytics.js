8e845eead0ab2e5251c2f103aba96df505805e38
Alex Grintsvayg <alex@flickswitch.cc>
2012-03-13 13:05:59 -0400

    oops

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,9 +1,7 @@
-<script>
-  var _gaq = [['_setAccount', 'UA-29865022-1'], ['_trackPageview']];
-  (function(d, t) {
-    var g = d.createElement(t),
-        s = d.getElementsByTagName(t)[0];
-    ga.src = ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g, s);
-  }(document, 'script'));
-</script>
+var _gaq = [['_setAccount', 'UA-29865022-1'], ['_trackPageview']];
+(function(d, t) {
+  var g = d.createElement(t),
+      s = d.getElementsByTagName(t)[0];
+  ga.src = ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
+  s.parentNode.insertBefore(g, s);
+}(document, 'script'));
