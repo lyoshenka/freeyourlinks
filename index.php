0e55b7d3c202d2fe99dedb13028453c0ee29310d
Alex Grin <lyoshenka@gmail.com>
2014-02-27 10:35:55 -0500

    not in debug anymore

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/vendor/silex/vendor/.composer/autoload.php';
 
 $app = new Silex\Application();
-$app['debug'] = true;
+//$app['debug'] = true;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path'       => __DIR__.'/views',
